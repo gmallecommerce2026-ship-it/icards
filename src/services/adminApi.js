7cd734d1c330f58a807b47448d0b9a1fe23d38ce
Nguyen Thien An <an.nguyenthien112802@gmail.com>
2026-03-30 16:18:47 +0700

    update

--- src/services/adminApi.js
+++ src/services/adminApi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const adminApi = axios.create({
   // URL này dành riêng cho backend của trang quản trị
-  baseURL: 'https://api-icards-dashboard.loca.lt/api/v1', 
+  baseURL: 'https://adminapi.icards.com.vn/api/v1', 
   headers: {
     'Bypass-Tunnel-Reminder': 'true',
     'Content-Type': 'application/json'
